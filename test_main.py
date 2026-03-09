1f58bae03aac5a9c37bbecfcf57343595437b450
chukkaladhanya <chukkaladhanya@gmail.com>
2026-03-09 14:51:07 +0530

    added new test cases

--- test_main.py
+++ test_main.py
@@ -7,9 +7,22 @@ def test_valid():
     response = test_app.get("/details")
     assert response.status_code == 200
 
-    response = test_app.get("/female")
+    response = test_app.get("/students/female")
     assert response.status_code == 200
 
+    response = test_app.get("/details?sort_by=age")
+    assert response.status_code == 200
+
+
 def test_invalid():
-    response = test_app.get("/m")
+    response = test_app.get("/students/m")
+    assert response.status_code == 400
+
+    response = test_app.get("/s")
+    assert response.status_code == 404
+
+    response = test_app.get("/details?sort_by=name")
+    assert response.status_code == 400
+
+    response = test_app.get("/delete_student/10000")
     assert response.status_code == 400
